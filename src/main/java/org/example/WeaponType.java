72928a10ce3fbea8cf1d5878dc6109eb71fe9d8e
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-02-08 14:52:42 +0100

    weapon type commit

---
+++ src/main/java/org/example/WeaponType.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public enum WeaponType {
+
+    SWORD (10),
+    MACE (5),
+    UNARMED (1);
+
+  private final int damage;
+
+    WeaponType(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+}
